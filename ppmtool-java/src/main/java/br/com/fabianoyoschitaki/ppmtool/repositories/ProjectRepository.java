aa67a617dc4462a3ef2d369144c4b6912f51cab5
fabianoyoschitaki <fabianoyoschitaki@gmail.com>
2019-02-10 23:23:53 -0200

    project and repository

---
+++ ppmtool-java/src/main/java/br/com/fabianoyoschitaki/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package br.com.fabianoyoschitaki.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fabianoyoschitaki.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+	
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
